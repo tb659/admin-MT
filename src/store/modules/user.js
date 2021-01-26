317c0c2426bd1085915eb88efcd81d402eea85d1
tb659 <tb541122@163.com>
2021-01-26 17:35:45 +0800

    路由请求数据

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -50,84 +50,7 @@ const user = {
     // 获取用户信息
     GetInfo({ commit, state }) {
       return new Promise((resolve, reject) => {
-        // getInfo(state.token).then(res => {
-        console.log('----------------------------获取用户信息接口')
-        let res = {
-          msg: '操作成功',
-          code: 200,
-          permissions: ['*:*:*'],
-          roles: ['admin'],
-          user: {
-            searchValue: null,
-            createBy: 'admin',
-            createTime: '2021-01-21 23:10:54',
-            updateBy: null,
-            updateTime: null,
-            remark: '管理员',
-            params: {},
-            userId: 1,
-            deptId: 103,
-            userName: 'admin',
-            nickName: '若依',
-            email: 'ry@163.com',
-            phonenumber: '15888888888',
-            sex: '1',
-            avatar: '',
-            salt: null,
-            status: '0',
-            delFlag: '0',
-            loginIp: '127.0.0.1',
-            loginDate: '2021-01-21T23:10:54.000+0800',
-            dept: {
-              searchValue: null,
-              createBy: null,
-              createTime: null,
-              updateBy: null,
-              updateTime: null,
-              remark: null,
-              params: {},
-              deptId: 103,
-              parentId: 101,
-              ancestors: null,
-              deptName: '研发部门',
-              orderNum: '1',
-              leader: '若依',
-              phone: null,
-              email: null,
-              status: '0',
-              delFlag: null,
-              parentName: null,
-              children: []
-            },
-            roles: [
-              {
-                searchValue: null,
-                createBy: null,
-                createTime: null,
-                updateBy: null,
-                updateTime: null,
-                remark: null,
-                params: {},
-                roleId: 1,
-                roleName: '超级管理员',
-                roleKey: 'admin',
-                roleSort: '1',
-                dataScope: '1',
-                menuCheckStrictly: false,
-                deptCheckStrictly: false,
-                status: '0',
-                delFlag: null,
-                flag: false,
-                menuIds: null,
-                deptIds: null,
-                admin: true
-              }
-            ],
-            roleIds: null,
-            postIds: null,
-            admin: true
-          }
-        }
+        getInfo(state.token).then(res => {
         const user = res.user
         const avatar = user.avatar == '' ? require('@/assets/images/profile.jpg') : process.env.VUE_APP_BASE_API + user.avatar
         if (res.roles && res.roles.length > 0) {
@@ -143,14 +66,14 @@ const user = {
       }).catch(error => {
         reject(error)
       })
-      // })
+      })
     },
 
     // 退出系统
     LogOut({ commit, state }) {
       return new Promise((resolve, reject) => {
-        // logout(state.token)
-        //   .then(() => {
+        logout(state.token)
+          .then(() => {
             commit('SET_TOKEN', '')
             commit('SET_ROLES', [])
             commit('SET_PERMISSIONS', [])
@@ -160,7 +83,7 @@ const user = {
           .catch(error => {
             reject(error)
           })
-      // })
+      })
     },
 
     // 前端 登出
